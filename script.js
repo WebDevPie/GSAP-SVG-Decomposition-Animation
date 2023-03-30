a1005204a39aa856aceae74eec93dae67f1dc9c7
WebDevPie <nazarmarchyshyn@gmail.com>
2023-03-30 12:40:54 +0300

    1-st commit

---
+++ script.js
@@ -0,0 +1,35 @@
+gsap.to("#dot", {
+    y: 60,
+    duration: 3,
+    delay: 1,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1
+});
+
+gsap.to("#a", {
+    y: 110,
+    duration: 3,
+    delay: 1,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1
+});
+
+gsap.to("#bg", {
+    y: 330,
+    duration: 3,
+    delay: 1,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1
+});
+
+gsap.to("#Subtract", {
+    y: 50,
+    duration: 3,
+    delay: 1,
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1
+});
